1e5cd534d002bb1d0a16c7e59a0c510a1f287bc4
Baguira <43543396+Luciern@users.noreply.github.com>
2018-09-27 18:33:47 +0200

    resubido

--- conexion.php
+++ conexion.php
@@ -2,4 +2,26 @@
 
 $conexion= new mysqli("localhost","root","","empresa");
 //var_dump($conexion);
+
+if($conexion->errno){
+	die("No hay conexion"-$conexion->error);
+	
+}
+	
+	$codigo = $_POST['contrasinal'];
+	$usuario= $_POST['nombre'];
+	
+	$consulta = "SELECT contrasinal,nombre FROM usuario WHERE contrasinal\"contrasinal\"";
+	$resultado = $conexion->query($consulta);
+	var_dump($resultado);
+
+	while($persona = $resultado->fetch_assoc()){
+		if($nombre == $persona['nombre']){
+			echo "Enhorabuena!!";
+			session_start();
+			S_SESSION['usuario'] = $codigo;
+			header("Location: index.php");
+		}
+	
+	}
 ?>
